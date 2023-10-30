410dbd2474ffc11dc2098e56bdaa7c8be28db758
Science-B <dmitriytwd@gmail.com>
2023-10-30 20:50:02 +0300

    added start-server-function

--- server/app.js
+++ server/app.js
@@ -1,23 +1,26 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import chalk from 'chalk';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth-routes.js';
 dotenv.config();
 
-mongoose
-	.connect(process.env.MONGO)
-	.then(() => {
-		console.log('Connected to db');
-	})
-	.catch((err) => {
-		console.log(err);
-	});
-
 const app = express();
+const PORT = 3000;
 app.use(express.json());
-
-app.listen(3000, () => {
-	console.log('Server is running on Port 3000');
-});
-
 app.use('/api/auth', authRouter);
+
+async function start() {
+	try {
+		await mongoose.connect(process.env.MONGO);
+		console.log(chalk.green(`MongoDB is working`));
+		app.listen(PORT, () => {
+			console.log(chalk.green(`Server is running on Port ${PORT}...`));
+		});
+	} catch (error) {
+		console.log(chalk.red(error.message));
+		process.exit(1);
+	}
+}
+
+start();
