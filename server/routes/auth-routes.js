efb55d9cb099ca201a890a7dfad9e9a7fe7cff50
Science-B <dmitriytwd@gmail.com>
2023-10-31 14:06:09 +0300

    created-middleware-for-signup-route

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import { check } from 'express-validator';
 
 import { signUp } from '../controllers/auth-controllers.js';
 
 const router = express.Router({ mergeParams: true });
 
-router.post('/signUp', signUp);
+router.post('/signUp', [
+	check('email', 'Email введён некорректно').isEmail(),
+	check('password', 'Минимальная длина пароля 8 символов').isLength({
+		min: 8,
+	}),
+	signUp,
+]);
 
 export default router;
