401c48e4966d1db089ea79bb05fcd9d4ea3a0626
Science-B <dmitriytwd@gmail.com>
2023-11-09 12:27:19 +0300

    added complete-comments-routes

---
+++ server/controllers/comments-controller.js
@@ -0,0 +1,28 @@
+import commentsService from '../services/comments-service';
+
+export const getComments = async (req, res) => {
+	try {
+		const list = await commentsService.get(req.query);
+		res.status(201).json(list);
+	} catch (error) {
+		handleError(500, 'SERVER_ERROR');
+	}
+};
+
+export const createComment = async (req, res) => {
+	try {
+		const newComment = await commentsService.create(req);
+		res.status(201).json(newComment);
+	} catch (error) {
+		handleError(500, 'SERVER_ERROR');
+	}
+};
+
+export const deleteComment = async (req, res) => {
+	try {
+		const deleteData = await commentsService.delete(req.params);
+		res.status(201).json(deleteData);
+	} catch (error) {
+		handleError(500, 'SERVER_ERROR');
+	}
+};
