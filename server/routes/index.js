9d13d3c7de5352cb23d4cb10e78cab47c458bd1a
Science-B <dmitriytwd@gmail.com>
2023-11-13 16:34:22 +0300

    added product-routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import authRoutes from './auth-routes.js';
 import commentsRoutes from './comments-routes.js';
+import productsRoutes from './products-routes.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/comments', commentsRoutes);
-router.use('/products', commentsRoutes);
+router.use('/products', productsRoutes);
 
 export default router;
